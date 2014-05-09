18a00243c55fae1d1e6627ae06b3cb0962e6f414
Josh Messer <joshuadmesser@gmail.com>
2014-05-09 17:53:45 -0400

    First inital commit with compile / watch gulp tasks in place

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,30 @@
 'use strict';
 
 var gulp = require('gulp')
+, stylus = require('gulp-stylus')
+, minify = require('gulp-minify-css')
 , nib = require('nib')
 , rupture = require('rupture')
 , typographic = require('typographic')
-, $ = require('gulp-load-plugins');
+
+, paths = {
+  coreFiles: './app/stylus/*.styl'
+}
 
 gulp.task('compile', function() {
-  gulp.src('./app/lcars/core.styl')
-    .pipe( $.stylus(
-        {
+  gulp.src('./app/stylus/lcars.styl')
+    .pipe(
+      stylus({
           use: [nib(), rupture(), typographic()]
-        }
-      ) )
-    .pipe( gulp.dest('./dist') );
+      })
+    )
+    .pipe( minify() )
+    .pipe( gulp.dest('./app/css') );
 });
+
+gulp.task('watch', function() {
+  gulp.watch(paths.coreFiles, ['compile']);
+});
+
+
+gulp.task('cCompile', ['compile', 'watch']);
